1cb1c07ffa1b6d82efc2864faf0d8c46d814cc96
Will Sams <will@samswebs.com>
2024-02-03 10:50:10 -0500

    Update index.js

--- src/db/seeds/development/index.js
+++ src/db/seeds/development/index.js
@@ -1,36 +1,57 @@
-const { readFile } = require('fs');
+import { readFile } from 'fs/promises';
 
-const jsonReader = (filepath, callback) => {
-  return new Promise((resolve, reject) => {
-    readFile(filepath, (error, data) => {
-      if (error) reject(error);
-      else {
-        const obj = JSON.parse(data);
-        callback(null, obj);
-        resolve();
-      }
-    });
-  });
+const jsonReader = async (filepath, callback) => {
+  try {
+    const data = await readFile(filepath);
+    const obj = JSON.parse(data);
+    callback(null, obj);
+    return obj;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+const dropAllConstraints = async (knex, tableName) => {
+  const tableExists = await knex.schema.hasTable(tableName);
+  if (tableExists) {
+    const constraintQuery = `
+      SELECT conname 
+      FROM pg_constraint 
+      WHERE conrelid = (
+        SELECT oid 
+        FROM pg_class 
+        WHERE relname = '${tableName}'
+      );
+    `;
+
+    const result = await knex.raw(constraintQuery);
+    const constraintNames = result.rows.map((row) => row.conname);
+    for (const constraintName of constraintNames) {
+      await knex.raw(`ALTER TABLE ${tableName} DROP CONSTRAINT IF EXISTS "${constraintName}";`);
+    }
+  }
 };
 
 const seedData = async (knex, tableName, data) => {
-  await knex(tableName).del();
+  if (tableName == 'rooms') await dropAllConstraints(knex, 'reservations');
+  const databaseExists = await knex.schema.hasTable(tableName);
+  if (databaseExists) await knex(tableName).del();
   return await knex(tableName).insert(data);
 };
 
 const injectTables = async (knex, tableName) => {
-  await jsonReader(`./seeds/development/${tableName}.json`, (error, data) => {
-    try {
+  try {
+    const data = await jsonReader(`./seeds/development/${tableName}.json`, (error, data) => {
       if (error) throw error;
-      else inserts = data;
-    } catch (ex) {
-      console.log(`Json file read failed: ${ex.message}`);
-    }
-  });
-  await seedData(knex, tableName, inserts);
+      else return data;
+    });
+    await seedData(knex, tableName, data);
+  } catch (ex) {
+    console.log(`Seed failed: ${ex.message}`);
+  }
 };
 
-exports.seed = async (knex) => {
+export const seed = async (knex) => {
   await injectTables(knex, 'rooms');
   await injectTables(knex, 'reservations');
 };
